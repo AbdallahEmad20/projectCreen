5582909259fd6585e8a12c6e4cdb720fd6677d37
AbdallahEmad20 <116479217+AbdallahEmad20@users.noreply.github.com>
2024-03-25 21:45:02 +0200

    Add files via upload

---
+++ js/Lang.js
@@ -0,0 +1,275 @@
+let translations = {
+    ar:{
+     
+  word1:"فتحنا لك أبواب العالم <br>  بمزايا استثنائية تتناسب  <br> مع ذوقك وقيمك ",
+  word2:"أنظم إلى عالم كرين creen وكن الجزء الأكثر اشراقا ",
+  word3:"حمل التطبيق علي اندرويد و ايفون الأن" ,
+  word4:"افضل تطبيق متكامل",
+  word5:"الترفيه يعتمد على",
+  word6:"الصحة",
+  word7:"بيئة صحية أمنه",
+  word8:"التجميل",
+  word9:"شكل مميز وجذاب",
+  word10:"الرياضة",
+  word11:"بنية جسدية مناسبة",
+  word12:"معنا",
+  word13:"تفتح لك افاق مميزة ",
+  word14:"حمل التطبيق الآن",
+  word15:"حمل التطبيق الآن",
+  word16:"المميزات",
+  word17:"تجربة اجتماعية رياضية غير مسبوقة ",
+  word18:"تواصل بسهولة مع مدربك المميز",
+  word19:"تواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميز",
+  word20:"امان عالي",
+  word21:"سياسة خصوصية محكمة",
+  word22:"محفظة خاصة  هدايا نقاط",
+  word23:" منتجات مميزة" ,
+  word24:"تواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميزتواصل بسهولة مع مدربك المميز",
+  word25:"حوافز",
+  word26:"تحديات",
+  word27:"اطلب طعامك ووصفاتك المفضلة ",
+  word28:"في دقائق",
+  word29:"عبّر عن نفسك بحرية وأمان في بيئة صحية",
+  word30:"تقدر التنوع و الإختلاف",
+  word31:"أحدث تقنيات التواصل والفائدة ",
+  word32:"طرق دفع امنة وميسرة",
+  word33: "مع كرين ودّع تعقيدات الدفع عبر الإنترنت! نقدم لك مجموعة متنوعة من وسائل الدفع الآمنة والموثوقة لتضمن لك تجربة شراء سهلة وسلسة دون أي قلق. ثق في كرين لعمليات دفع آمنة وراحة بال!",
+  word34:"كل برامجك الرياضية بين يديك الان ",
+  word35:"اعرف رحلتك في الرياضة والتغذية للحصول إلى الهدف ",
+  word36:"عزز تعاملك الرياضي ",
+  word37:"ابحث عن مدربين رياضيين وأخصائيي تغذية محترفين بالقرب منك، واحجز جلساتك بسهولة لتحقيق أهدافك.",
+  word38:"اعرف رحلتك في الرياضة والتغذية  للحصول إلى الهدف ",
+  word39:"جرب الخيارات المتاحة الاكثر تفاعلا وذكاء ",
+  word40:"ابحث عن الخيار الأمثل من مدربين وأخصائيي تغذية موثوقين، وقارن الخيارات لتجد الأنسب لاحتياجاتك.",
+  word41:"خليك مع كرين في امان على الدوام",
+  word42:"رحلة واحدة فقط فيها محطات من أروع  المتع ",
+  word43:"انضم إلى رحلتنا نحو عالم أفضل مع مستخدمينا <br> من جميع أنحاء العالم.",
+  word44:"أنضم إلى عالم كرين creen",
+  word45:"وكن الجزء الأكثر اشراقا ",
+  word46:"الشجاعة في التجربة وزينة العقل في حسن الاختيار ",
+  word47:"حمل التطبيق الآن",
+  word48:"ضع أهدافك الرياضية وخططك الغذائية، وتابع تقدمك مع إحصائيات مخصصة لتحقيق النجاح.     ",
+  word49:"جميع التعليقات",
+  word50:"رحلة مميزة مع كرين",
+  word51:"انطباعات مستخدمين ",
+  word52:"كرين",
+  word53:"تطبيق مميز يلبي كل احتياجاتك",
+  word54:"التواصل",
+  word55:"شروط الخدمة",
+  word56:"سياسة الخصوصية",
+  word57:"©️ كل الحقوق محفوظة الي كرين",
+  wordbtn:"حمل التطبيق الان"
+    },
+
+    en:{
+        word1:"We opened the doors of the world to you with exceptional benefits that suit your taste and values",
+        word2:"Join the world of Creen and be the brightest part",
+        word3:"Download the application on Android and iPhone now",
+        word4:"The best integrated applicationEntertainment depends on",
+        word5:"Entertainment depends on",
+        word6:"the health",
+        word7:"A safe, healthy environment",
+        word8:"Beautification",
+        word9:"Distinctive and attractive shape",
+       word10:"Sports",
+       word11:"Suitable physical structure",
+        word12:"With us",
+        word13:"It opens unique horizons for you",
+        word14:"Download App ",
+        word15:"Download App",
+       word16:"Advantages",
+       word17:"An unprecedented social sports experience",
+        word18:" Easily communicate with your distinguished coach",
+        word19:"Communicate easily with your distinguished coach. Communicate easily with your distinguished coach. CommunicateEasily with your distinguished trainer",
+        word20:"High security",
+        word21:"Solid privacy policy ",
+        word22:"  Special wallet gift points",
+        word23:" Featured products",
+        word24:"Communicate easily with your distinguished coach. Communicate easily with your distinguished coach. CommunicateEasily with your distinguished trainer",
+
+        word25:" Incentives",
+        word26:"Challenges",
+        word27:"Order your favorite food and recipes",
+        word28:"In minutes",
+        word29:"Express yourself freely and safely in a healthy environment",
+        word30:"Appreciate diversity and difference",
+        word31:"The latest communication ",
+       word32: "Safe and easy payment methods",
+       word33:"With Crane, say goodbye to the complications of paying online! We offer you a variety of safe and reliable payment methods to ensure an easy and smooth purchasing experience without any worries. Trust Crane for secure payments and peace of mind!",  
+       word34:  " All your sports programs are in your hands no",
+       word35: "  Know your journey in sports and nutrition to reach your goa", 
+       word36:  "  Enhance your sporty handling",
+       word37:" Find professional sports trainers and nutritionists near you, and easily book your sessions to achieve your goals.",  
+       word38:"Know your journey in sports and nutrition to reach your goal",  
+       word39:" Set your sports goals and nutritional plans, and track your progress ",  
+       word40:"Experience the most interactive and intelligent options available",  
+       word41: " Stay safe with Crane always", 
+       word42: "  Only one trip contains stations of the most wonderful pleasures", 
+       word43:" Join our journey towards a better world with our users <br> from all over the world.",  
+       word44: "  Join the world of creen", 
+       word45: " And be the brightest part", 
+       word46: "  Courage in experience and adornment of mind in making good choices", 
+       word47: " Download App", 
+       word48:"Set your sports goals and nutritional plans, ",
+       word49:"All comments",
+       word50:"A special trip with Crane",
+       word51:"User impressions",
+       word52:"creen",
+word53:"A unique application that meets all your needs",
+word54:"communication",
+word55:"Terms of Service",
+word56:"privacy policy",
+word57:"©️ All rights reserved to Crane",
+wordbtn:"Download App"
+    }
+
+   
+  
+
+
+
+
+
+
+    
+    
+
+    
+    
+    
+   
+
+}
+
+// ======
+
+
+var titleHero =document.querySelector(".titleHero")
+const LanguageSelector =document.getElementById("Language-selector")
+var AllSections =document.querySelectorAll(".Mysec")
+
+var mainImage1 =document.querySelector(".mainImage1")
+var mainImage2 =document.querySelector(".mainImage2")
+var secialDivSecTwo =document.querySelector(".secialDivSecTwo")
+var contnetStyleone =document.querySelectorAll(".contnetStyleone")
+var contnetStyleTwo =document.querySelectorAll(".contnetStyleTwo")
+var titlleContent =document.querySelector(".titlleContent")
+var changeFont =document.querySelectorAll(".changeFont")
+var changeFontpara=document.querySelectorAll(".changeFontpara")
+var paratwelve =document.querySelector(".paratwelve")
+var texttwelve =document.querySelector(".texttwelve")
+var titlesecele=document.querySelectorAll(".titlesecele")
+var paragsecel=document.querySelectorAll(".paragsecele")
+var titleseceight =document.querySelector(".titleseceight")
+var optoion1 =document.querySelector(".optoion1")
+var optoion2 =document.querySelector(".optoion2")
+
+
+LanguageSelector.addEventListener("change" , function (e){
+setLanguage (e.target.value)
+localStorage.setItem("language", e.target.value)
+})
+
+document.addEventListener("DOMContentLoaded" , ()=>{
+    const MyLanguage =localStorage.getItem("language")
+    setLanguage(MyLanguage)
+})
+
+ function setLanguage(language){
+    const elements =document.querySelectorAll("[data-lang]")
+
+    elements.forEach(element =>{
+
+       const  translateKey =element.getAttribute("data-lang")
+        element.innerHTML =translations[language][translateKey]
+        
+    })
+
+    if(language === "ar" ){
+
+
+    AllSections.forEach(section=>{
+        section.dir="rtl"
+
+
+    })
+    contnetStyleone.forEach(speak=>{
+        speak.style.fontSize="49px" 
+     })
+
+     contnetStyleTwo.forEach(speak=>{
+        speak.style.fontSize="20px" 
+     })
+     changeFont.forEach(change=>{
+        change.style.fontSize="31px"
+     })
+     titlesecele.forEach(titlesecc=>{
+        titlesecc.style.fontSize="31px"
+        titlesecc.style.textAlign="right"
+     })
+  
+
+     paragsecel.forEach(para=>{
+        para.style.fontSize="16px"
+        para.style.textAlign="right"
+
+     })
+
+     mainImage1.style.display="block"
+mainImage2.style.display="none"
+titlleContent.style.textAlign="right"
+
+        titleHero.style.fontSize="61px"
+        secialDivSecTwo.style.justifyContent="left"
+paratwelve.style.fontSize="49px"
+texttwelve.style.fontSize="25px"
+titleseceight.style.textAlign="right"
+
+
+
+
+    }else{
+
+
+        
+        AllSections.forEach(section=>{
+            section.dir="ltr"
+
+        })
+        contnetStyleone.forEach(speak=>{
+            speak.style.fontSize="30px" 
+         })
+
+         contnetStyleTwo.forEach(speak=>{
+            speak.style.fontSize="14px" 
+         })
+
+         changeFont.forEach(change=>{
+            change.style.fontSize="22px"
+         })
+         titlesecele.forEach(title=>{
+            title.style.fontSize="24px"
+        title.style.textAlign="left"
+
+         })
+    
+         paragsecel.forEach(para=>{
+            para.style.fontSize="12px"
+        para.style.textAlign="left"
+
+         })
+      
+mainImage1.style.display="none"
+mainImage2.style.display="block"
+titleHero.style.fontSize="40px"
+  
+    secialDivSecTwo.style.justifyContent="right"
+    titlleContent.style.textAlign="left"
+    paratwelve.style.fontSize="29px"
+    texttwelve.style.fontSize="21px"
+    titleseceight.style.textAlign="center"
+
+    }
+
+
+}
